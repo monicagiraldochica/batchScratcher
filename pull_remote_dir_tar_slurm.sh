755f88fece7cef16d3b3a4c3e6b176b58efd8fcf
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-07-01 11:18:31 -0500

    Read remoteParen and remoteBase in only one ssh connection

--- pull_remote_dir_tar_slurm.sh
+++ pull_remote_dir_tar_slurm.sh
@@ -321,13 +321,16 @@ pull_remote_dir_tar_slurm() {
 
   echo "remoteDirPth: ${remoteDirPth}"
 
-  if [ -z "${remoteHost}" ]; then
+  if [[ -z "${remoteHost}" ]]; then
     local p="${remoteDirPth%/}"
     remoteParent="$(dirname -- "$p")" || return 3
     remoteBase="$(basename -- "$p")" || return 3
   else
-    remoteParent="$(ssh -o BatchMode=yes "${remoteHost}" "bash -lc 'p=$(printf %q "${remoteDirPth}"); p=\${p%/}; dirname -- \"\$p\"'")" || return 3
-    remoteBase="$(ssh -o BatchMode=yes "${remoteHost}" "bash -lc 'p=$(printf %q "${remoteDirPth}"); p=\${p%/}; basename -- \"\$p\"'")" || return 3
+    #remoteParent="$(ssh -o BatchMode=yes "${remoteHost}" "bash -lc 'p=$(printf %q "${remoteDirPth}"); p=\${p%/}; dirname -- \"\$p\"'")" || return 3
+    #remoteBase="$(ssh -o BatchMode=yes "${remoteHost}" "bash -lc 'p=$(printf %q "${remoteDirPth}"); p=\${p%/}; basename -- \"\$p\"'")" || return 3
+    read -r remoteParent remoteBase < <(
+        ssh -o BatchMode=yes "$remoteHost" "bash -lc 'p=$(printf %q "${remoteDirPth}"); p=\${p%/}; printf \"%s %s\n\" \"\$(dirname -- \"\$p\")\" \"\$(basename -- \"\$p\")\"'"
+      ) || return 3
   fi
 
   echo "remoteParent: ${remoteParent}"
